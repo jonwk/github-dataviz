0011c2cb4fd43116f962b0965e2f17152f42b1b1
john-kommala <johnkommala@gmail.com>
2021-12-26 16:21:00 +0000

    Finished Lang Chart UI

--- sample-app/components/Charts.js
+++ sample-app/components/Charts.js
@@ -5,16 +5,20 @@ import { Section } from "../styles";
 import { PieChartComp, BarChartComp } from "../components/charts/";
 import { ResponsiveContainer } from "recharts";
 import { mockRepoData } from "../mock";
+import { langColors } from "../util";
+// import { error } from "console";
 // const Charts = ({ repostarChartData }) => {
-const ChartWidth = 300;
-const ChartHeight = 300;
 
 // Create Most Starred chart
 
 // const Charts = ({repoData}) => {
 const Charts = () => {
+  const ChartWidth = 300;
+  const ChartHeight = 300;
   const [starChartData, setStarChartData] = useState([{}]);
-  const [starChartConfig, setStarChartConfig] = useState(null);
+  const [starChartConfig, setStarChartConfig] = useState({});
+  const [langChartData, setLangChartData] = useState([{}]);
+  const [langChartConfig, setLangChartConfig] = useState({});
 
   const repoData = mockRepoData;
   const initStarChart = () => {
@@ -61,18 +65,151 @@ const Charts = () => {
       console.log(starBarChartConfig);
       setStarChartConfig(starBarChartConfig);
       console.log(starBarChartConfig);
-    //   console.log(starBarChartConfig);
+      //   console.log(starBarChartConfig);
+    }
+  };
+
+  const getLangMap = (repoData) => {
+    const langMap = new Map();
+    for (let index = 0; index < repoData.length; index++) {
+      const repo = repoData[index];
+      if (repo.language) {
+        if (langMap.size != 0) {
+          const count = langMap.get(repo.language);
+          if (!count) {
+            langMap.set(repo.language, 1);
+          } else {
+            langMap.set(repo.language, count + 1);
+          }
+        } else {
+          langMap.set(repo.language, 1);
+        }
+      }
+    }
+    return langMap;
+  };
+
+  const mapToArray = (map) => {
+    let array = Array.from(map, ([key, value]) => ({ key, value }));
+    return array.filter((x) => x !== undefined);
+  };
+
+  const addColors = (langArr) => {
+    let langDataWithColors = [];
+
+    for (let i = 0; i < langArr.length; i++) {
+      const key = langArr[i].key;
+      const value = langArr[i].value;
+      const color = langColors[langArr[i].key.toLowerCase()];
+
+      langDataWithColors.push({ lang: key, repos: value, color: color });
+    }
+    return langDataWithColors;
+  };
+
+  const getLangData = (repoData) => {
+    const convData = mapToArray(getLangMap(repoData));
+    const langData = addColors(convData);
+    return langData;
+  };
+
+  const getRandomColor = () => {
+    var letters = "0123456789ABCDEF";
+    var color = "#";
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  };
+
+  const colorsArr = (langData) => {
+    // let colors = Array.from(langData, ({ key, value, color }) => color);
+    // colors = colors.map(v => (v === undefined ? getRandomColor() : v));
+    // colors = colors.filter(x => x !== undefined);
+    // colors = colors.map(color => {
+    //   var len = color == null ? 0 : color.length;
+    //   if (len > 0) {
+    //     return `#${len > 4 ? color.slice(1) : color.slice(1).repeat(2)}FF`;
+    //   }
+    // });
+    return Array.from(langData, ({ key, value, color }) => color)
+      .map((v) => (v === undefined ? getRandomColor() : v))
+      .filter((x) => x !== undefined)
+      .map((color) => {
+        var len = color == null ? 0 : color.length;
+        if (len > 0) {
+          return `#${len > 4 ? color.slice(1) : color.slice(1).repeat(2)}CF`;
+        }
+      });
+  };
+
+  const initLangChart = () => {
+    const langData = getLangData(repoData);
+    setLangChartData(langData);
+    const labels = langData.map((lang) => lang.label);
+    const data = langData;
+    const sectorColors = colorsArr(langData);
+    // setLangChartData(data);
+
+    if (data.length > 0) {
+      const width = ChartWidth;
+      const height = ChartHeight;
+      const margin = {
+        top: 0,
+        right: 0,
+        left: 0,
+        bottom: 0,
+      };
+
+      const cx = 150;
+      const cy = 150;
+      const innerRadius = 70;
+      const outerRadius = 120;
+      const paddingAngle = 0.5;
+      const dataKey = "repos";
+      const pieChartConfig = {
+        width,
+        height,
+        margin,
+        cx,
+        cy,
+        innerRadius,
+        outerRadius,
+        paddingAngle,
+        data,
+        dataKey,
+        sectorColors,
+      };
+      console.log("langChartConfig - ", pieChartConfig);
+      setLangChartConfig(pieChartConfig);
+      console.log("langChartConfig - ", pieChartConfig);
     }
   };
 
   useEffect(() => {
     if (repoData.length) {
-      //   initLangChart();
       initStarChart();
+      initLangChart();
       console.log(starChartData);
+      console.log(langChartData);
       //   initThirdChart();
     }
   }, []);
+
+  const errorHandler = (obj) => {
+    for (const prop in obj) {
+      if (
+        obj.hasOwnProperty(prop) &&
+        (obj[prop] === undefined || obj[prop] === null)
+      ) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  const langChartError = errorHandler(langChartConfig);
+  const starChartError = errorHandler(starChartConfig);
 
   return (
     <Section dark>
@@ -83,7 +220,8 @@ const Charts = () => {
           </header>
 
           <div className="chart-container">
-            <PieChartComp />
+            {langChartError && <p>Nothing to see here!</p>}
+            <PieChartComp config={langChartConfig} />
           </div>
         </div>
         <div className="chart">
@@ -94,6 +232,7 @@ const Charts = () => {
           <div className="chart-container">
             {/* <BarChartComp config={starBarChartConfig} /> */}
             {/* {starBarChartConfig.starChartData && ( */}
+            {starChartError && <p>Nothing to see here!</p>}
             <BarChartComp config={starChartConfig} />
             {/* )} */}
 
@@ -107,7 +246,9 @@ const Charts = () => {
           </header>
 
           <div className="chart-container">
-            <PieChartComp />
+            {/* <PieChartComp /> */}
+            {langChartError && <p>Nothing to see here!</p>}
+            <PieChartComp config={langChartConfig} />
 
             {/* {langChartError && <p>Nothing to see here!</p>} */}
             {/* <canvas id="langChart" width={chartSize} height={chartSize} /> */}
--- sample-app/components/charts/PieChartComp.js
+++ sample-app/components/charts/PieChartComp.js
@@ -1,17 +1,18 @@
 import React, { useCallback, useState } from "react";
+import PropTypes from "prop-types";
 import { PieChart, Pie, Sector, Tooltip, Cell } from "recharts";
 
-const data = [
-  { lang: "Java", repos: 5 },
-  { lang: "VHDL", repos: 1 },
-  { lang: "JavaScript", repos: 4 },
-  { lang: "SCSS", repos: 1 },
-  { lang: "Jupyter Notebook", repos: 1 },
-  { lang: "Python", repos: 4 },
-  { lang: "CSS", repos: 1 },
-  { lang: "Dart", repos: 1 },
-  { lang: "Verilog", repos: 1 },
-];
+// const data = [
+//   { lang: "Java", repos: 5 },
+//   { lang: "VHDL", repos: 1 },
+//   { lang: "JavaScript", repos: 4 },
+//   { lang: "SCSS", repos: 1 },
+//   { lang: "Jupyter Notebook", repos: 1 },
+//   { lang: "Python", repos: 4 },
+//   { lang: "CSS", repos: 1 },
+//   { lang: "Dart", repos: 1 },
+//   { lang: "Verilog", repos: 1 },
+// ];
 
 const renderActiveShape = (props) => {
   const RADIAN = Math.PI / 180;
@@ -86,29 +87,25 @@ const renderActiveShape = (props) => {
   );
 };
 
-// const colors = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "#e1109a"];
-// const colors = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "#e1109a"];
-// const colors = [
-//   "rgba(255, 99, 132, 1)",
-//   "rgba(54, 162, 235, 1)",
-//   "rgba(255, 206, 86, 1)",
-//   "rgba(153, 102, 255, 1)",
-//   "rgba(75, 192, 192, 1)",
-//   "rgba(255, 159, 64, 1)",
-// ];
-const colors = [
-  "#b07219B3",
-  "#adb2cbB3",
-  "#f1e05aB3",
-  "#c6538cB3",
-  "#00C49F",
-  "#3572A5B3",
-  "#563d7cB3",
-  "#00B4ABB3",
-  "#b2b7f8B3",
-];
+const PieChartComp = ({ config }) => {
+  const {
+    width,
+    height,
+    margin,
+    cx,
+    cy,
+    innerRadius,
+    outerRadius,
+    paddingAngle,
+    data,
+    dataKey,
+    sectorColors,
+  } = config;
+  // const {  } = dimensions;
+  console.log(config);
+  // dimensions = {cx:150,cy:150,innerRadius:70,outerRadius:120,paddingAngle}
+  // const colors = data.map({lang,repos,colors}={color})
 
-export default function PieChartComp() {
   const [activeIndex, setActiveIndex] = useState(0);
   const onPieEnter = useCallback(
     (_, index) => {
@@ -116,57 +113,37 @@ export default function PieChartComp() {
     },
     [setActiveIndex]
   );
+
   return (
-    <PieChart
-      width={300}
-      height={300}
-      margin={{
-        top: 0,
-        right: 0,
-        left: 0,
-        bottom: 10,
-      }}
-      overflow="visible"
-    >
-      {/* <Tooltip /> */}
+    <PieChart width={width} height={height} margin={margin} overflow="visible">
       <Pie
-        stroke="none"
         activeIndex={activeIndex}
         activeShape={renderActiveShape}
         data={data}
-        cx={150}
-        cy={150}
-        innerRadius={70}
-        outerRadius={120}
-        fill="transparent"
-        paddingAngle={0.5}
-        dataKey="repos"
+        cx={cx}
+        cy={cy}
+        innerRadius={innerRadius}
+        outerRadius={outerRadius}
+        paddingAngle={paddingAngle}
+        dataKey={dataKey}
         onMouseEnter={onPieEnter}
+        stroke="none"
+        fill="transparent"
       >
-        {data.map((entry, index) => (
-          <Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
-        ))}
+        {data &&
+          data.map((entry, index) => (
+            <Cell
+              key={`cell-${index}`}
+              fill={sectorColors[index % sectorColors.length]}
+            />
+          ))}
       </Pie>
     </PieChart>
   );
-}
+};
 
-// backgroundColor: Array(9)
+PieChartComp.propTypes = {
+  config: PropTypes.object.isRequired,
+};
 
-// length: 9
-// [[Prototype]]: Array(0)
-// borderColor: Array(9)
-// 0: "#b07219"
-// 1: "#adb2cb"
-// 2: "#f1e05a"
-// 3: "#c6538c"
-// 4: "undefined"
-// 5: "#3572A5"
-// 6: "#563d7c"
-// 7: "#00B4AB"
-// 8: "#b2b7f8"
-// length: 9
-// [[Prototype]]: Array(0)
-// chartType: "pie"
-// ctx: canvas#langChart.chartjs-render-monitor
-// data: Array(9)
+export default PieChartComp;
