9d5e6ab4e0caac63335600fa2845b893372be5a8
john-kommala <johnkommala@gmail.com>
2021-12-31 15:53:33 +0000

    Removed unnecesary files

--- githubviz/UI/index.js
+++
@@ -1,3 +0,0 @@
-import UserInfo from "./UserInfo";
-import Charts from "./Charts";
-export { UserInfo, Charts };
--- githubviz/mock/index.js
+++
@@ -1,5 +0,0 @@
-import mockLangData from "./mockLangData";
-import mockRepoData from "./mockRepoData";
-import mockUserData from "./mockUserData";
-
-export { mockLangData, mockRepoData, mockUserData };
